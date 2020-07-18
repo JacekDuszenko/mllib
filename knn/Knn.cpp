eebf98f6bc52db6b5c447df1b2c5700778452c78
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-07-18 03:37:29 +0200

    finished cpp shared object

--- knn/Knn.cpp
+++ knn/Knn.cpp
@@ -1,39 +1,42 @@
 #include "Knn.h"
-#include "DataDimensionsMismatch.h"
 
-#include <iostream>
-#include <utility>
 #include <numeric>
 #include <cmath>
 
+#define min(A, B) (A > B ? A : B)
+
 Mllib::Knn::Knn(int vectorDimension, int k) : vector_dimension(vectorDimension), k(k) {}
 
-void Mllib::Knn::IngestData(Mllib::DataPoints dps, Mllib::Classes clss) {
-    validateDimensions(dps, clss);
+void Mllib::Knn::IngestData(Mllib::DataPoints dps) {
     this->data_points = std::move(dps);
-    this->classes = std::move(clss);
 }
 
-void Mllib::Knn::validateDimensions(const Mllib::DataPoints& xs, const Mllib::Classes& ys) {
-    if (xs.size() != ys.size()) throw DataDimensionsMismatch(xs.size(), ys.size());
+std::function<bool(std::vector<double> &, std::vector<double> &)> euclid(const std::vector<double> &vec) {
+    auto comparator_function = [&vec](std::vector<double> &fst, std::vector<double> &snd) {
+        double fst_d = 0.0;
+        double snd_d = 0.0;
+        for (int i = 0; i < fst.size(); ++i) {
+            fst_d += pow(vec.at(i) - fst.at(i), 2);
+            snd_d += pow(vec.at(i) - snd.at(i), 2);
+        }
+        return sqrt(fst_d) > sqrt(snd_d);
+    };
+    return comparator_function;
 }
 
-
-int Mllib::Knn::FindKnn(std::vector<double> vec) {
-    auto cmp = euclidianMetric(vec);
-    std::sort(data_points.begin(),data_points.end(), cmp);
+std::vector<double> Mllib::Knn::FindKnn(std::vector<double> &vec) {
+    auto cmp = euclid(vec);
+    std::sort(data_points.begin(), data_points.end(), cmp);
+    int iter = 0;
+    std::vector<double> avg(data_points[0].size(), 0.0);
+    int iterElems = min(data_points.size(), k);
+    while (iter < iterElems) {
+        auto &elem = data_points.at(iter);
+        for (int i = 0; i < elem.size(); ++i) {
+            avg[i] += elem[i];
+        }
+        ++iter;
+    }
+    std::transform(avg.begin(), avg.end(), avg.begin(), [iterElems](auto &e) { return e / iterElems; });
+    return avg;
 }
-
-bool (*Mllib::Knn::euclidianMetric())(std::vector<double> vec) {
-
-    return [vec](std::vector<double> fst, std::vector<double> snd) {
-            double fst_d = 0.0;
-            double snd_d = 0.0;
-            for (int i =0;i<fst.size();++i) {
-                fst_d += pow(vec.at(i) - fst.at(i), 2);
-                snd_d += pow(vec.at(i) - snd.at(i), 2);
-            }
-            return sqrt(fst_d) > sqrt(snd_d);
-        };
-}
-
--- knn/Knn.h
+++ knn/Knn.h
@@ -6,23 +6,19 @@
 
 namespace Mllib {
     typedef std::vector<std::vector<double>> DataPoints;
-    typedef std::vector<int> Classes;
 
     class Knn {
         int vector_dimension;
         int k;
         DataPoints data_points;
-        Classes classes;
-
-        static void validateDimensions(const DataPoints &vector, const Classes &vector1);
 
 
     public:
         Knn(int vectorDimension, int k);
 
-        void IngestData(DataPoints dps, Classes clss);
+        void IngestData(DataPoints dps);
 
-        int FindKnn(std::vector<double> vec);
+        std::vector<double> FindKnn(std::vector<double>& vec);
 
         virtual ~Knn();
 
